b98e2df6e2b278b180fe64cc5e2a6593d8fb6286
MacOS <macos@MacOSs-Mac.local>
2019-02-26 07:09:08 -0800

    Initial Commit

---
+++ Whats The Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by MacOS on 2/26/19.
+//  Copyright © 2019 MacOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
